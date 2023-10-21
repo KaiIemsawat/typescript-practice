d3693ddeb8f7538306823f04f531c05a3921c0f6
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-21 18:03:27 -0400

    ts with react

---
+++ ts-react/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+    return (
+        <div className="App">
+            <Header title="Hello, this was created with TS" color="#93d589" />
+        </div>
+    );
+}
+
+export default App;
