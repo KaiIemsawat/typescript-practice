ab7ce563eb8f77083dab27ef9f61ad05a07de8b7
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-03-20 22:00:18 -0400

    undirectional data flow, callback function

--- ts-react-tailwind/todo-refresh/src/App.tsx
+++ ts-react-tailwind/todo-refresh/src/App.tsx
@@ -2,13 +2,25 @@ import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/todos";
 
 function App() {
+    const setTodoCompleted = (id: number, completed: boolean) => {
+        alert(
+            `Todo with id ${id} is now ${
+                completed ? "completed" : "not completed"
+            }`
+        );
+    };
+
     return (
         <main className="py-10 h-screen space-y-5">
             <h1 className="font-bold text-center text-3xl">Todo</h1>
             <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
                 <div className="space-y-2">
                     {dummyData.map((todo) => (
-                        <TodoItem todo={todo} />
+                        <TodoItem
+                            key={todo.id}
+                            todo={todo}
+                            onCompletedChange={setTodoCompleted}
+                        />
                     ))}
                 </div>
             </div>
