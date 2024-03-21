438328eef8a5f303b304d2996b2fd2aa63a9c455
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-03-20 21:49:46 -0400

    added some styles

---
+++ ts-react-tailwind/todo-refresh/src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todos";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "title 1",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "title 2",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "title 3",
+        completed: true,
+    },
+];
